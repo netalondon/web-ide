31f10d31e289ba724467e2daf93346da3db49627
Neta London <neta.london@gmail.com>
2023-12-04 15:56:11 +0200

    Updates

--- static/js/328.2f89c89d.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunk_nand2tetris_web=self.webpackChunk_nand2tetris_web||[]).push([[328],{2328:(e,s,t)=>{t.r(s),t.d(s,{About:()=>n,default:()=>d});var a=t(7878);const n=()=>(0,a.jsx)("span",{children:"To be added later"}),d=n}}]);
-//# sourceMappingURL=328.2f89c89d.chunk.js.map
---
+++ static/js/70.3efa1b6d.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_nand2tetris_web=self.webpackChunk_nand2tetris_web||[]).push([[70],{2070:(e,s,t)=>{t.r(s),t.d(s,{About:()=>n,default:()=>d});var a=t(864);const n=()=>(0,a.jsx)("span",{children:"To be added later"}),d=n}}]);
